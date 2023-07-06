5e9b91eb19b847b023989a9b1fc5aab143a59613
Alejandro Nuñez Herrera <alejandro2191947@correo.uis.edu.co>
2023-07-05 23:38:48 -0500

    se añadio los componentes de home de los diferentes roles y la redireccion a ellos

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,31 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { LoginService } from '../login.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
   correoElectronico: string = '';
   contrasena: string = '';
 
-  constructor(private loginService: LoginService) {}
+  constructor(private loginService: LoginService, private router: Router) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onSubmit() {
-    this.loginService.login(this.correoElectronico, this.contrasena)
-      .subscribe({
-        next: response => {
-          // El inicio de sesión fue exitoso, hacer algo con la respuesta
-          console.log('Inicio de sesión exitoso:', response);
-        },
-        error: error => {
-          // Ocurrió un error durante el inicio de sesión
-          console.error('Contraseña incorrecta. Error durante el inicio de sesión:', error);
+    this.loginService.login(this.correoElectronico, this.contrasena).subscribe({
+      next: (response) => {
+        // El inicio de sesión fue exitoso, hacer algo con la respuesta
+        console.log('Inicio de sesión exitoso:', response);
+        if (response === 4) {
+          // Redirigir al componente deseado para el rol 1
+          this.router.navigate(['/homeAdmin']); 
+        };
+        if (response ===3) {
+          // Redirigir al componente deseado para el rol 2
+          this.router.navigate(['/homeMedico']); 
+        };
+        if (response === 1) {
+          // Redirigir al componente deseado para el rol 3
+          this.router.navigate(['/homeUsuario']); 
         }
-      });
+      },
+      error: (error) => {
+        // Ocurrió un error durante el inicio de sesión
+        console.error(
+          'Contraseña incorrecta. Error durante el inicio de sesión:',
+          error
+        );
+      },
+    });
   }
 }
